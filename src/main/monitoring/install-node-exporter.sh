a8e1d96e43e05799585384545943167c78bce9c6
deeshu <divyadevivasudevan@gmail.com>
2023-05-28 15:31:13 +0530

    monitoruing

---
+++ src/main/monitoring/install-node-exporter.sh
@@ -0,0 +1,6 @@
+sudo -i
+wget https://github.com/prometheus/node_exporter/releases/download/v*/node_exporter-*.*-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+cd node_exporter-*.*-amd64
+chmod +x node_exporter
+./node_exporter
---
+++ src/main/monitoring/install-prometheus.sh
@@ -0,0 +1,4 @@
+sudo -i
+wget https://github.com/prometheus/prometheus/releases/download/v*/prometheus-*.*-amd64.tar.gz
+tar xvf prometheus-*.*-amd64.tar.gz
+cd prometheus-*.*
